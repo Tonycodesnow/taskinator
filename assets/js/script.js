c95d874a747cc4bc5de2d3449a27bb496e3e587a
Ant <huertinez.family@outlook.com>
2021-08-17 10:50:24 -0700

    add task update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,11 @@
-console.log(window.document);
-
 var buttonEl = document.querySelector("#save-task");
-var taskToDoEl = document.querySelector("#tasks-to-do");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-}
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
 
 buttonEl.addEventListener("click", createTaskHandler);
-
-// buttonEl.addEventListener("click", function() {
-//     var listItemEl = document.createElement("li");
-//     listItemEl.className = "task-item";
-//     listItemEl.textContent = "This is a new task.";
-//     tasksToDoEl.appendChild(listItemEl);
-// });
-
-
