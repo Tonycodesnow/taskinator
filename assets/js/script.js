808babaf7e55f34db36894bf60d461e5ba45b818
Ant <huertinez.family@outlook.com>
2021-08-16 19:58:03 -0700

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+console.log(window.document);
+
+var buttonEl = document.querySelector("#save-task");
+var taskToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
+
+// buttonEl.addEventListener("click", function() {
+//     var listItemEl = document.createElement("li");
+//     listItemEl.className = "task-item";
+//     listItemEl.textContent = "This is a new task.";
+//     tasksToDoEl.appendChild(listItemEl);
+// });
+
+
